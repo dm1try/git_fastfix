f236389cdecab9ca392853955f2bb9d3d9e305fb
dm1try <me@dmitry.it>
2020-04-05 22:03:05 +0300

    incapsulate shell call helpers

--- lua/git_fastfix/init.lua
+++ lua/git_fastfix/init.lua
@@ -11,17 +11,25 @@ local nvim_buf_set_lines = vim.api.nvim_buf_set_lines
 local nvim_get_current_line = vim.api.nvim_get_current_line
 local split = vim.split
 
+local function nvim_shell_call(command)
+  return nvim_call_function('system', { command })
+end
+
+local function nvim_term_open(command)
+  return nvim_call_function('termopen', { command })
+end
+
 function FixupToTheCommit()
   local commit_hash = split(nvim_get_current_line(), ' ')[1]
-  nvim_call_function('system', { "git commit --fixup " .. commit_hash })
-  nvim_call_function('system', { "GIT_EDITOR=true git rebase -i --autosquash --autostash " .. commit_hash .. "^" })
+  nvim_shell_call('git commit --fixup ' .. commit_hash)
+  nvim_shell_call('GIT_EDITOR=true git rebase -i --autosquash --autostash ' .. commit_hash .. '^')
   nvim_command("echo 'Patch applied to " .. nvim_get_current_line() .. "'")
   nvim_command("close")
 end
 
 function ShowGitLog()
   local buf = nvim_create_buf(false, true)
-  local git_log_output = nvim_call_function('system', {'git log -n 10 --oneline'})
+  local git_log_output = nvim_shell_call('git log -n 10 --oneline')
   local git_logs = split(git_log_output, "\n")
 
   nvim_buf_set_lines(buf, 0, -1, true, git_logs)
@@ -40,7 +48,7 @@ function OpenGitFastFixWindow()
   }
   local win = nvim_open_win(buf, 0, opts)
   nvim_set_current_win(win)
-  nvim_call_function('termopen', {'git add --patch ' .. source_code_filename})
+  nvim_term_open('git add --patch ' .. source_code_filename)
   nvim_command('startinsert')
   nvim_command(":autocmd TermClose <buffer> :lua ShowGitLog()")
 end
