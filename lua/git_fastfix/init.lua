c19e63530a2c0d12418fe60bbd5edfe1f1154d0c
dm1try <me@dmitry.it>
2020-04-05 21:51:57 +0300

    import functions to local scope

--- lua/git_fastfix/init.lua
+++ lua/git_fastfix/init.lua
@@ -1,33 +1,46 @@
+local nvim_win_set_config = vim.api.nvim_win_set_config
+local nvim_open_win = vim.api.nvim_open_win
+local nvim_win_set_buf = vim.api.nvim_win_set_buf
+local nvim_create_buf = vim.api.nvim_create_buf
+local nvim_call_function = vim.api.nvim_call_function
+local nvim_set_current_win = vim.api.nvim_set_current_win
+local nvim_command = vim.api.nvim_command
+local nvim_buf_get_name = vim.api.nvim_buf_get_name
+local nvim_buf_set_keymap = vim.api.nvim_buf_set_keymap
+local nvim_buf_set_lines = vim.api.nvim_buf_set_lines
+local nvim_get_current_line = vim.api.nvim_get_current_line
+local split = vim.split
+
 function FixupToTheCommit()
-  local commit_hash = vim.split(vim.api.nvim_get_current_line(), ' ')[1]
-  vim.api.nvim_call_function('system', { "git commit --fixup " .. commit_hash })
-  vim.api.nvim_call_function('system', { "GIT_EDITOR=true git rebase -i --autosquash --autostash " .. commit_hash .. "^" })
-  vim.api.nvim_command("echo 'Patch applied to " .. vim.api.nvim_get_current_line() .. "'")
-  vim.api.nvim_command("close")
+  local commit_hash = split(nvim_get_current_line(), ' ')[1]
+  nvim_call_function('system', { "git commit --fixup " .. commit_hash })
+  nvim_call_function('system', { "GIT_EDITOR=true git rebase -i --autosquash --autostash " .. commit_hash .. "^" })
+  nvim_command("echo 'Patch applied to " .. nvim_get_current_line() .. "'")
+  nvim_command("close")
 end
 
 function ShowGitLog()
-  local buf = vim.api.nvim_create_buf(false, true)
-  local git_log_output = vim.api.nvim_call_function('system', {'git log -n 10 --oneline'})
-  local git_logs = vim.split(git_log_output, "\n")
+  local buf = nvim_create_buf(false, true)
+  local git_log_output = nvim_call_function('system', {'git log -n 10 --oneline'})
+  local git_logs = split(git_log_output, "\n")
 
-  vim.api.nvim_buf_set_lines(buf, 0, -1, true, git_logs)
-  vim.api.nvim_win_set_buf(0, buf)
-  vim.api.nvim_buf_set_keymap(buf, 'n', '<CR>' ,':lua FixupToTheCommit()<cr>',{})
+  nvim_buf_set_lines(buf, 0, -1, true, git_logs)
+  nvim_win_set_buf(0, buf)
+  nvim_buf_set_keymap(buf, 'n', '<CR>' ,':lua FixupToTheCommit()<cr>',{})
 end
 
 function OpenGitFastFixWindow()
-  local source_code_filename = vim.api.nvim_buf_get_name(0)
+  local source_code_filename = nvim_buf_get_name(0)
 
-  local buf = vim.api.nvim_create_buf(false, true)
+  local buf = nvim_create_buf(false, true)
   local opts = {
     relative='cursor',
     width=80, height=20,
     col=0, row=1, anchor='NW', style='minimal'
   }
-  local win = vim.api.nvim_open_win(buf, 0, opts)
-  vim.api.nvim_set_current_win(win)
-  vim.api.nvim_call_function('termopen', {'git add --patch ' .. source_code_filename})
-  vim.api.nvim_command('startinsert')
-  vim.api.nvim_command(":autocmd TermClose <buffer> :lua ShowGitLog()")
+  local win = nvim_open_win(buf, 0, opts)
+  nvim_set_current_win(win)
+  nvim_call_function('termopen', {'git add --patch ' .. source_code_filename})
+  nvim_command('startinsert')
+  nvim_command(":autocmd TermClose <buffer> :lua ShowGitLog()")
 end
